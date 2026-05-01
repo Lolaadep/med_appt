c8b87f16e1051a2d0c1120e9d097c84d138eb9f4
Lolaadep <aademikanra@gmail.com>
2026-05-01 08:55:27 -0400

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from './Components/Navbar/Navbar'
 import LandingPage from './Components/Landing_Page/LandingPage' // 👈 add this
-import logo from '.Components/mylogo.png'
 
 function App() {
   return (
