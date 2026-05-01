eb25e34173b981106b58f8033347a2ecb8c83ecc
Lolaadep <aademikanra@gmail.com>
2026-05-01 07:04:45 -0400

    Initial Commit

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:hFbq1HouS45MeCE1bI30zW8j@172.21.132.34:";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
