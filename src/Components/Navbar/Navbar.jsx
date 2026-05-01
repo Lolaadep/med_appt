e2ac37cda544318a62897222e7af6f25ee526b8c
Lolaadep <aademikanra@gmail.com>
2026-05-01 16:24:12 -0400

    Initial Commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,40 +1,30 @@
-import "./Navbar.css";
-function Navbar() {
-  return (
-    <section id="section-header">
-      <div className="container">
-        <header className="site-header">
-          <div className="logo-container">
-            <img
-              src="mylogo.png"
-              alt="StayHealthy Logo"
-              className="logo"
-            />
-          </div>
+import React from "react";
+import { Link } from "react-router-dom";
 
-          <nav className="main-nav">
-            <a href="../Landing_Page/LandingPage.html" className="nav-btn">
-              Home
-            </a>
-            <a href="#" className="nav-btn">
-              Appointments
-            </a>
-            <a href="#" className="nav-btn">
-              Records
-            </a>
-            <a href="#" className="nav-btn">
-              Reviews
-            </a>
-            <a href="../SignUP/SignUp.html" className="nav-btn">
-              Sign Up
-            </a>
-            <a href="../Login/Login.html" className="nav-btn">
-              Log In
-            </a>
-          </nav>
-        </header>
-      </div>
-    </section>
+function Navbar() {
+  const user = localStorage.getItem("user");
+
+  const handleLogout = () => {
+    localStorage.removeItem("user");
+    alert("Logged out");
+  };
+
+  return (
+    <div>
+      <h2>StayHealthy</h2>
+
+      <Link to="/">Home</Link> | 
+      <Link to="/search">Appointments</Link> | 
+
+      {!user ? (
+        <>
+          <Link to="/login">Login</Link> | 
+          <Link to="/">Sign Up</Link>
+        </>
+      ) : (
+        <button onClick={handleLogout}>Logout</button>
+      )}
+    </div>
   );
 }
 
